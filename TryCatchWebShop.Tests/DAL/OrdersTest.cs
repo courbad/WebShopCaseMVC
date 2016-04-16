bf994c46e1d05d2c1cd00b86b80748325b688b2d
Māris Cakuls <zeme@outlook.com>
2016-04-17 01:52:27 +0300

    Added (fixed) basic DB test;

--- TryCatchWebShop.Tests/DAL/OrdersTest.cs
+++ TryCatchWebShop.Tests/DAL/OrdersTest.cs
@@ -1,10 +1,9 @@
-﻿using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TryCatchWebShop;
-using TryCatchWebShop.Controllers;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TryCatchWebShop.DAL;
-using System;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using System;
 
 namespace TryCatchWebShop.Tests.DAL
 {
@@ -16,17 +15,27 @@ namespace TryCatchWebShop.Tests.DAL
         {
             var inOrder = new Order()
             {
-                Id = Guid.NewGuid(),
                 Address = "addrezs",
                 City = "riga",
-                Email = "adsf@adfs.com",
+                Email = "hey@maris.com",
                 FirstName = "maris",
                 LastName = "also maris",
                 HouseNumber = "666",
-                Title = "Royal Highness",
-                ZipCode = "1010"
-
+                Title = "MR",
+                ZipCode = "1010",
             };
+
+            inOrder.Products = new List<Product>()
+            {
+                new Product()
+                {
+                    Count = 10,
+                    Id = "MD5something",
+                    Name = "Test product",
+                    Price = 99m
+                }
+            };
+
 
             using (WebShopContext db = new WebShopContext())
             {
@@ -34,15 +43,33 @@ namespace TryCatchWebShop.Tests.DAL
                 db.SaveChanges();
             }
 
+
             Order outOrder = null;
             using (WebShopContext db = new WebShopContext())
             {
-                var all = db.Orders.ToList();
-                outOrder = db.Orders.Single(o => o.Id == inOrder.Id);
+                outOrder = db.Orders
+                    .Include("Products")
+                    .Single(o => o.Id == inOrder.Id);
             }
 
             Assert.IsNotNull(outOrder);
-        
+            Assert.AreEqual(inOrder.Id, outOrder.Id);
+            Assert.AreEqual(inOrder.Title, outOrder.Title);
+            Assert.AreEqual(inOrder.FirstName, outOrder.FirstName);
+            Assert.AreEqual(inOrder.LastName, outOrder.LastName);
+            Assert.AreEqual(inOrder.Address, outOrder.Address);
+            Assert.AreEqual(inOrder.City, outOrder.City);
+            Assert.AreEqual(inOrder.ZipCode, outOrder.ZipCode);
+            Assert.AreEqual(inOrder.HouseNumber, outOrder.HouseNumber);
+            Assert.AreEqual(inOrder.Email, outOrder.Email);
+
+            Assert.IsNotNull(outOrder.Products);
+            Assert.IsTrue(outOrder.Products.Count() == 1);
+            Assert.AreEqual(inOrder.Products.First().Id, outOrder.Products.First().Id);
+            Assert.AreEqual(inOrder.Products.First().Guid, outOrder.Products.First().Guid);
+            Assert.AreEqual(inOrder.Products.First().Name, outOrder.Products.First().Name);
+            Assert.AreEqual(inOrder.Products.First().Price, outOrder.Products.First().Price);
+
         }
     }
 }
